dc213a045102029517113a71254efd889cdd83a7
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-11-24 10:14:46 +0000

    restructure routes and project folders, add endpoints description

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const cors = require("cors");
 const express = require("express");
-const authRouter = require("./routes/auth.route");
-const registerRouter = require("./routes/register.route");
+const apiRouter = require("./routes/api.route");
 const app = express();
 require("./db/connection");
 
@@ -9,8 +8,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/auth", authRouter);
-app.use("/register", registerRouter);
+app.use("/api", apiRouter);
 
 app.all("*", (req, res) => {
   res.status(404).send({ error: "Sorry, that page does not exist" });
--- routes/register.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const registerUser = require("../controllers/register.controller");
-const registerRouter = express.Router();
-
-registerRouter.post("/", registerUser);
-
-module.exports = registerRouter;
